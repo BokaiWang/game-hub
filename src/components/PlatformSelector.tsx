eb02a4cd3810ba18058a8f28c6c65bf7d3013f2d
BokaiWang <bk.wang1989@gmail.com>
2023-06-06 10:21:20 -0400

    Fetched platforms using react-query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -12,7 +12,7 @@ const PlatformSelector = ({
   onSelectedPlatform,
   selectedPlatform,
 }: platformSelectorProps) => {
-  const { data: platforms, error } = usePlatforms();
+  const { data, error } = usePlatforms();
 
   if (error) {
     return null;
@@ -24,7 +24,7 @@ const PlatformSelector = ({
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {platforms.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectedPlatform(platform)}
             key={platform.id}
